74539a4cfe8968630d1bf40b207438c782cb4586
mopy <mopy7@users.noreply.github.com>
2026-03-10 17:06:33 +0000

    feat: add loader animation for weather fetch
    Improve UX by adding a loader animation during API requests.
    
    - Show loader while fetching weather data
    - Hide previous weather results during loading
    - Display results after successful response

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const form = document.getElementById("search-form");
 const cityInput = document.getElementById("city-input");
+const weatherResult = document.getElementById("weather-result")
 
 const cityName = document.getElementById("city-name");
-const weatherIcon = document.querySelector("#weather-result img")
+const loader = document.querySelector(".loader");
+const weatherIcon = document.querySelector("#weather-result img");
 const temperature = document.getElementById("temperature");
 const condition = document.getElementById("condition");
 const humidity = document.getElementById("humidity");
@@ -14,19 +16,25 @@ form.addEventListener("submit", (e) => {
 
   const city = cityInput.value.trim();
   if (!city) return;
-  
+
   const apiKey = "b652a5d3a52e479495c100522261003";
   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+
+  loader.style.display = "block";
+  weatherResult.style.display = "none";
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      loader.style.display = "none";
+
       if (data.error) {
         alert(data.error.message);
         return;
       }
 
-      // console.log(data);
+      weatherResult.style.display = "flex";
+
       cityName.textContent = data.location.name;
       weatherIcon.src = data.current.condition.icon;
       temperature.textContent = `${data.current.temp_c}°C`;
